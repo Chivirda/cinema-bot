4a2bfe2113701c268b052b3f920e95c80182108e
Sergey Chivirda <si.chivirda@gmail.com>
2024-05-02 17:55:46 +0300

    Добавил базу и пару запросов

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+import sqlite3 from "sqlite3";
+
+export class Data {
+    db = new sqlite3.Database("src/data/chinook.db", err => {
+        if (err) console.error(err);
+        console.log("Connected to SQLite database");
+    });
+
+    tables() {
+        this.db.all("SELECT name FROM sqlite_schema WHERE type = 'table' AND name NOT LIKE 'sqlite_%'", [], (err, rows) => {
+            if (err) console.error(err);
+            console.log(rows);
+        });
+    }
+
+    customers() {
+        this.db.all("SELECT COUNT(CustomerId) FROM customers", [], (err, rows) => {
+            if (err) console.error(err);
+            console.log(rows);
+        });
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import "dotenv/config";
 import { Telegraf } from "telegraf";
-import { message } from "telegraf/filters";
+import { Data } from "./data/data.js";
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply("Hi!"));
+class CinemaBot {
+    constructor() {
+        this.bot = new Telegraf(process.env.BOT_TOKEN);
+        this.bot.start((ctx) => ctx.reply("Hi!"));
+        this.bot.hears("таблицы", (ctx) => {
+            console.log("Index:", new Data().tables());
+        })
+        this.bot.hears("customers", (ctx) => {
+            console.log("Index:", new Data().customers());
+        })
+        this.bot.launch();
+    }
+}
+
+new CinemaBot();
